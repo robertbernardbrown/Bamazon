06e550e5a714bb04b07091d21cc483b0c4946f1b
Bob Brown <robertbernardbrown@gmail.com>
2018-02-12 12:09:09 -0500

    fetches data from db and displays in format in console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,8 +13,20 @@ connection.connect();
    
 connection.query("SELECT * FROM products", function (error, results) {
 	if (error) throw error;
-	console.log("Data received from Db:\n");
-	console.log(results);
+
+	console.log("Welcome! Here are all the items for sale:");
+	for (var i = 0; i < results.length; i++) {
+		var element = results[i];
+		var itemId = element.item_id;
+		var productName = element.product_name;
+		var departmentName = element.department_name;
+		var price = element.price;
+		var quantity = element.stock_quantity;
+
+		console.log("ID: " + itemId + " | Product: " + productName + " | Department: " + departmentName + " | Price: $" + price + " | Stock:" + quantity);
+		
+	}
+
 });
    
 connection.end();
