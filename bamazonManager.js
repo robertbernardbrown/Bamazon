cf61781830879067f032005fb782d98622136836
Bob Brown <robertbernardbrown@gmail.com>
2018-02-16 13:10:36 -0500

    reduces inquirer prompts for adding a product into one inquirer query. Also puts switch case work into functions so that it reads clearer

--- bamazonManager.js
+++ bamazonManager.js
@@ -35,15 +35,13 @@ function managerPrompt () {
 			deptArr.push(element.department_name);
 		}
 
-		inquirer.prompt([{
-			type: "input",
-			name: "productName",
-			message: "Please enter the name of the product you would like to add:",
-		}]).then( answers => {
-
-			let brandNewItem = answers.productName;
-                
-			inquirer.prompt([{
+		inquirer.prompt([
+			{
+				type: "input",
+				name: "productName",
+				message: "Please enter the name of the product you would like to add:",
+			},
+			{
 				type: "input",
 				name: "productCost",
 				message: "Please enter a price for this product:",
@@ -53,77 +51,136 @@ function managerPrompt () {
 					}
 					return "Please enter a valid number";
 				}
-			}]).then( answers => {
-    
-				let brandNewItemCost = parseInt(answers.productCost);
-                    
-				inquirer.prompt([{
-					type: "input",
-					name: "productStock",
-					message: "Please enter the quantity for this product:",
-					validate: function (input) {
-						if (isNaN(input) === false) {
-							return true;
-						}
-						return "Please enter a valid number";
+			},
+			{
+				type: "input",
+				name: "productStock",
+				message: "Please enter the quantity for this product:",
+				validate: function (input) {
+					if (isNaN(input) === false) {
+						return true;
 					}
-				}]).then( answers => {
-        
-					let brandNewItemQuant = parseInt(answers.productStock);
+					return "Please enter a valid number";
+				}
+			},
+			{
+				type: "list",
+				name: "productDept",
+				message: "Finally, please enter a department for this product:",
+				choices: [
+					"Produce",
+					"Poultry",
+					"Dry Goods",
+					"Desserts",
+					"Frozen"
+				]
+			}
+		]).then( answers => {
+
+			let brandNewItem = answers.productName;
+			let brandNewItemCost = parseInt(answers.productCost);        
+			let brandNewItemQuant = parseInt(answers.productStock);            
+			let brandNewItemDept = answers.productDept;
                         
-					inquirer.prompt([{
-						type: "list",
-						name: "productDept",
-						message: "Finally, please enter a department for this product:",
-						choices: [
-							"Produce",
-							"Poultry",
-							"Dry Goods",
-							"Desserts",
-							"Frozen"
-						]
-					}]).then( answers => {
+			console.log("Product draft:\n==============\n" + 
+				"Product Name: " + brandNewItem + 
+				"\nCost: " 	 	 + brandNewItemCost + 
+				"\nStock: " 	 + brandNewItemQuant + 
+				"\nDepartment: " + brandNewItemDept + "\n==============");
             
-						let brandNewItemDept = answers.productDept;
-                        
-						console.log("Product draft:\n==============\n" + 
-						"Product Name: " + brandNewItem + 
-						"\nCost: " 	 	 + brandNewItemCost + 
-						"\nStock: " 	 + brandNewItemQuant + 
-						"\nDepartment: " + brandNewItemDept + "\n==============");
-            
-						inquirer.prompt([{
-							type: "confirm",
-							name: "newProductConfirm",
-							message: "Is everything correct?",
-						}]).then( answers => {
+			inquirer.prompt([{
+				type: "confirm",
+				name: "newProductConfirm",
+				message: "Is everything correct?",
+			}]).then( answers => {
                 
-							let brandNewItemConfirm = answers.newProductConfirm;
-							let brandNewItemSales = 0;
+				let brandNewItemConfirm = answers.newProductConfirm;
+				let brandNewItemSales = 0;
 
-							if (brandNewItemConfirm) {
-								connection.query("INSERT INTO bamazon_db.products" + 
-												" SET product_name = ?, department_name = ?, price = ?, stock_quantity = ?, product_sales = ?", 
-								[brandNewItem, brandNewItemDept, brandNewItemCost ,brandNewItemQuant, brandNewItemSales], 
-								(error) => {
-									if (error) throw error;
-									console.log("New product processed");
-									fillItemArr();
-									managerPrompt();
-								});
-
-							} else {
-								newProduct();
-							}
-                            
-						});
-
+				if (brandNewItemConfirm) {
+					connection.query("INSERT INTO bamazon_db.products" + 
+									" SET product_name = ?, department_name = ?, price = ?, stock_quantity = ?, product_sales = ?", 
+					[brandNewItem, brandNewItemDept, brandNewItemCost ,brandNewItemQuant, brandNewItemSales], 
+					(error) => {
+						if (error) throw error;
+						console.log("New product processed");
+						fillItemArr();
+						managerPrompt();
 					});
-        
-				});
-    
+
+				} else {
+					newProduct();
+				}
+                            
 			});
 
+		});
+	}
+
+	function viewProducts (arr) {
+		console.log("Manager View\n============\nItems for sale:");
+		console.table(arr);
+		managerPrompt();
+	}
+
+	function viewLowProducts (arr) {
+		console.log("Manager View\n============\nLow Inventory:");
+		var lowInv = [];
+		for (let j = 0; j < arr.length; j++) {
+			if (arr[j].stock_quantity < 5) {
+				lowInv.push(arr[j]);
+			}
+		}
+		console.table(lowInv);
+		managerPrompt();
+	}
+
+	function addToProducts (arr) {
+		console.table(arr);
+		inquirer.prompt([{
+			type: "input",
+			name: "managersChoice",
+			message: "To which item would you like to add (please enter item id):",
+			validate: function (input) {
+				if (isNaN(input) === false && parseInt(input) > 0 && parseInt(input) <= arr.length) {
+					return true;
+				}
+				return "Please enter a valid number";
+			}
+		}]).then( answers => {
+
+			let choice = parseInt(answers.managersChoice);
+                
+			inquirer.prompt([{
+				type: "input",
+				name: "managersAmount",
+				message: "How much inventory would you like to add:",
+				validate: function (input) {
+					if (isNaN(input) === false) {
+						return true;
+					}
+					return "Please enter a valid number";
+				}
+			}]).then( answers => {
+
+				let item = arr[choice - 1];
+				let choiceAmount = parseInt(answers.managersAmount);
+				let newAmount = choiceAmount + item.stock_quantity;
+                    
+				connection.query("UPDATE bamazon_db.products" + 
+									" SET stock_quantity = ?" + 
+									" WHERE item_id = ?" 
+					,[newAmount, choice],(error) => {
+					if (error) throw error;
+					console.log("Command processed\n=================\n" + 
+									"Item: " 		   + item.product_name + 
+									"\nNew quantity: " + newAmount + 
+									"\n=================");
+					fillItemArr();
+					managerPrompt();
+				});
+			});
+                
 		});
 	}
 
@@ -144,82 +201,20 @@ function managerPrompt () {
     
 		switch (input) {
 		case "View products for sale":
-			console.log("Manager View\n============\nItems for sale:");
-			console.table(productArr);
-			managerPrompt();
+			viewProducts(productArr);
 			break;
-            
 		case "View low inventory":
-			console.log("Manager View\n============\nLow Inventory:");
-			var lowInv = [];
-			for (let j = 0; j < productArr.length; j++) {
-				if (productArr[j].stock_quantity < 5) {
-					lowInv.push(productArr[j]);
-				}
-			}
-			console.table(lowInv);
-			managerPrompt();
-			break;
-            
+			viewLowProducts(productArr);
+			break; 
 		case "Add to inventory":
-			console.table(productArr);
-			inquirer.prompt([{
-				type: "input",
-				name: "managersChoice",
-				message: "To which item would you like to add (please enter item id):",
-				validate: function (input) {
-					if (isNaN(input) === false && parseInt(input) > 0 && parseInt(input) <= productArr.length) {
-						return true;
-					}
-					return "Please enter a valid number";
-				}
-			}]).then( answers => {
-
-				let choice = parseInt(answers.managersChoice);
-                
-				inquirer.prompt([{
-					type: "input",
-					name: "managersAmount",
-					message: "How much inventory would you like to add:",
-					validate: function (input) {
-						if (isNaN(input) === false) {
-							return true;
-						}
-						return "Please enter a valid number";
-					}
-				}]).then( answers => {
-
-					let item = productArr[choice - 1];
-					let choiceAmount = parseInt(answers.managersAmount);
-					let newAmount = choiceAmount + item.stock_quantity;
-                    
-					connection.query("UPDATE bamazon_db.products" + 
-									" SET stock_quantity = ?" + 
-									" WHERE item_id = ?" 
-						,[newAmount, choice],(error) => {
-						if (error) throw error;
-						console.log("Command processed\n=================\n" + 
-									"Item: " 		   + item.product_name + 
-									"\nNew quantity: " + newAmount + 
-									"\n=================");
-						fillItemArr();
-						managerPrompt();
-					});
-				});
-                
-			});
-			break;
-            
+			addToProducts(productArr);
+			break; 
 		case "Add new product":
-
-			newProduct();
+			newProduct(productArr);
 			break;
-
 		case "Quit":
-        
 			connection.end();
-			break;
-            
+			break; 
 		default:
 			return "Please choose an option";
 		}});
